b3ab6b816bfb083016231f0853c8ed7758ec1d29
harsha-kande <f20191027@hyderabad.bits-pilani.ac.in>
2025-02-17 23:41:12 +0530

    resolved cors error

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"KandeHarsha/handler"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	// CORSS middleware
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	r.Use(cors.New(config))
 
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
